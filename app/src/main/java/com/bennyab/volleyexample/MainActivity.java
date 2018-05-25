a0565943ab677c6263c73dddcb25f74af82c2bb9
Benjamin Aballay <bennya82@gmail.com>
2018-05-25 10:52:00 +0300

    added waiting dialog (boca)

--- app/src/main/java/com/bennyab/volleyexample/MainActivity.java
+++ app/src/main/java/com/bennyab/volleyexample/MainActivity.java
@@ -1,8 +1,16 @@
 package com.bennyab.volleyexample;
 
+import android.graphics.Color;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,13 +31,24 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     TextView txt;
+    AlertDialog waitdialog;
+    
+    public AlertDialog setProgressDialog(){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setView(R.layout.waiting_dialog);
+        waitdialog = builder.create();
+        waitdialog.show();
+        return  waitdialog;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         txt = findViewById(R.id.txtTest);
-
+        setProgressDialog();
         RequestQueue queue = Volley.newRequestQueue(this);
         String contactsurl = getString(R.string.url_get_contacts);
         final GetContactsRequest request = new GetContactsRequest(Request.Method.GET, contactsurl,
@@ -37,24 +56,27 @@ public class MainActivity extends AppCompatActivity {
                 new Response.Listener<JSONArray>() {
                     @Override
                     public void onResponse(JSONArray response) {
-                        Log.v("onResponse",response.toString());
+                        Log.v("Logger","data recieved");
 
                         Gson gson = new Gson();
                         Contact[] result = gson.fromJson(response.toString(),Contact[].class);
 
-                        ArrayList<Contact> data = GetContactsRequest.parseContacts(response);
+//                        ArrayList<Contact> data = GetContactsRequest.parseContacts(response);
+                        Log.v("Logger","after parse");
 
-
-                        Toast.makeText(MainActivity.this,Integer.toString(data.size()),Toast.LENGTH_LONG).show();
+                        Toast.makeText(MainActivity.this,Integer.toString(result.length),Toast.LENGTH_LONG).show();
+                        waitdialog.dismiss();
                     }
                 },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Log.e("onErrorResponse",error.getMessage());
+                        //Log.e("onErrorResponse",error.getMessage());
+                        waitdialog.dismiss();
                     }
                 });
 
+        Log.v("Logger","before request");
         queue.add(request);
     }
 }
